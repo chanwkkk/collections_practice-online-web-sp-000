604a89221e827a8b3785b57622488bd9237dbb6c
Yingqi Chen <chanwk18@hotmail.com>
2019-03-27 06:41:32 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -27,3 +27,11 @@ end
 def reverse_array(array)
   array.reverse
 end
+
+def kesha_maker(array)
+  array.each do |string|
+    letters=string.split('')
+    letters[2]='$'
+    letters.join
+  end
+end
