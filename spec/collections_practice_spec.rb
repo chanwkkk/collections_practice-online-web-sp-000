2b0cd822e5e04caf4dcef71f6a249b99e9657e7a
Yingqi Chen <chanwk18@hotmail.com>
2019-03-27 06:05:00 +0000

    Automatically backed up by Learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -32,7 +32,7 @@ describe 'collections practice' do
 
   describe '#swap_elements_from_to' do
     it 'swap two elements in an array'
-      expect(swap_elements_from_to([1,2,3,4,5])).to eq([1,3,2,4,5])
+      expect(swap_elements_from_to([1,2,3,4,5]),1,2).to eq([1,3,2,4,5])
   end
 
   # Question 5
